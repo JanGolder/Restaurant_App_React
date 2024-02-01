5b61954798bad36131f838c28e4f81c25f7e76ac
Jan Golder <kontakt.jangolder@gmail.com>
2024-02-01 15:18:07 +0100

    add structure of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/USerProgressContext";
+
 function App() {
   return (
-    <>
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart/>
+        <Checkout/>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
